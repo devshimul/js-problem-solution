760a8f17a04385f145b12bbfce7f3a2e9f1ec411
devshimul <shimul.bisd@gmail.com>
2024-12-03 12:33:20 +0600

    Odd average numbers

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function inchToFeet(inch) {
   return `Person is ${feet} feet & ${inchExtra} inch`;
 }
 const personFeet = inchToFeet(95);
-console.log(personFeet);
+// console.log(personFeet);
 
 // Miles to kilometer converter
 function mileToKm(miles) {
@@ -13,7 +13,7 @@ function mileToKm(miles) {
   return km;
 }
 const kilometer = mileToKm(69);
-console.log(kilometer);
+// console.log(kilometer);
 
 function isLeapYear(year) {
   if (year % 4 === 0) {
@@ -23,4 +23,25 @@ function isLeapYear(year) {
   }
 }
 let leapYear = isLeapYear(2032);
-console.log(leapYear);
+// console.log(leapYear);
+
+const numbers = [11, 265, 144, 273, 99, 154, 268, 197, 333, 49, 22];
+
+function odds(numbers) {
+  let oddNumbers = [];
+  for (const number of numbers) {
+    if (number % 2 === 1) {
+      oddNumbers.push(number);
+    }
+  }
+  let oddLength = oddNumbers.length;
+  let sum = 0;
+  for (const oddNum of oddNumbers) {
+    sum = sum + oddNum;
+  }
+  const avgOddResult = sum / oddLength;
+  return avgOddResult;
+}
+
+const oddAvg = odds(numbers);
+console.log(parseInt(oddAvg));
