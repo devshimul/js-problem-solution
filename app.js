7aa01f15a5851b1e255261f3cacc9bbda05eff9c
devshimul <shimul.bisd@gmail.com>
2024-12-03 11:37:11 +0600

    Meter to km convert

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 function inchToFeet(inch) {
-  const feet = inch / 12;
-  return feet;
+  let feet = inch / 12;
+  feet = parseInt(feet);
+  let inchExtra = inch % 2;
+  return `Person is ${feet} feet & ${inchExtra} inch`;
 }
-const personFeet = inchToFeet(78);
+const personFeet = inchToFeet(95);
 console.log(personFeet);
+
+// Miles to kilometer converter
+function mileToKm(miles) {
+  let km = miles * 1.6;
+  return km;
+}
+const kilometer = mileToKm(69);
+console.log(kilometer);
